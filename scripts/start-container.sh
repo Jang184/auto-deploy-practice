d152d0498fc31270d5cfc58e7a2e64607a103a53
jang184 <e2joo418@gmail.com>
2024-12-03 22:14:36 +0900

    ecr 로그인 명령어 수정

--- scripts/start-container.sh
+++ scripts/start-container.sh
@@ -4,7 +4,7 @@
 source /home/ec2-user/.env
 
 # AWS ECR 로그인
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin ${ECR_REGISTRY}.dkr.ecr.ap-northeast-2.amazonaws.com/${ECR_REPOSITORY}
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin ${ECR_REGISTRY}.dkr.ecr.ap-northeast-2.amazonaws.com
 
 # ECR에서 이미지 다운로드 및 실행
 docker pull ${ECR_REGISTRY}.dkr.ecr.ap-northeast-2.amazonaws.com/${ECR_REPOSITORY}:latest
